3f3fbb93c44e5bd591ad73d1861043d9f0dae7bf
anaxisa <ramosanay@my.easternct.edu>
2018-12-14 13:09:34 -0500

    Update app and add progress bar

--- app.R
+++ app.R
@@ -10,25 +10,20 @@ ui <- fluidPage(# Create title for app
   sidebarLayout(
     # Panel for user input
     sidebarPanel(
-      # Input: Entrez key (if applicable)
-      textInput("entrezK", "Input NCBI API key:"),
-      helpText(
-        "Note: The NCBI API key is not required, but will make searching faster.",
-        "For more information, please visit",
-        tags$a(href = "https://www.ncbi.nlm.nih.gov/books/NBK25497/", "NCBI.")
-      ),
+      h4(strong("Step 1: Enter PMIDs using method (A) or (B)"), style = "color:slategrey"),
       
       # Input: PMID text list
-      textInput("txt", "Input list of PMIDs:"),
+      textAreaInput("txt", "(A) Paste/input list of PMIDs:", height = "150px"),
       helpText(
-        "Note: Please ensure that PMIDs are comma-seperated, followed by a space.",
-        "Example: 123, 124"
+        "Note: PMIDs can be input as a comma-seperated list OR one PMID can be listed per line."
       ),
+      
+      h4(strong("-OR-"), align = "center", style = "color:red"),
       
       # Input: CSV file - FIX server code
       fileInput(
         "file1",
-        "Choose CSV file:",
+        "(B) Upload CSV file:",
         multiple = FALSE,
         accept = c("text/csv",
                    "text/plain")
@@ -37,42 +32,72 @@ ui <- fluidPage(# Create title for app
       # Horizontal line
       tags$hr(),
       
+      # Input: checkbox for additional return of results
+      h4(strong("Step 2: Additional information"), style = "color:slategrey"),
+      checkboxGroupInput(
+        "SorT",
+        "Return information from Source or Target?:",
+        c("Source" = "source",
+          "Target" = "target")
+      ),
+      checkboxGroupInput(
+        "choices",
+        "Include following in edge list:",
+        c(
+          "Authors" = "authors",
+          "Title" = "title",
+          "Publication Date" = "pubdate",
+          "Journal Name" = "jname"
+        )
+      ),
+      
+      # Horizontal line
+      tags$hr(),
+      
+      # Input: Entrez key (if applicable)
+      h4(strong("Step 3: (Optional)"), style = "color:slategrey"),
+      textInput("entrezK", "Input NCBI API key:"),
+      helpText(
+        "Note: The NCBI API key is not required, but will make searching faster.",
+        "For more information, please visit",
+        tags$a(href = "https://www.ncbi.nlm.nih.gov/books/NBK25497/", "NCBI.")
+      ),
+      
+      tags$hr(),
+      
       # Input: Action button to submit
+      h4(strong("Step 4: Submit!"), style = "color:slategrey"),
       actionButton("action", "Submit")
     ),
     
     # Main panel for downloading results
     mainPanel(
-      tableOutput("resultsum"),
-      
-      # Horizontal line
-      tags$hr(),
-      
-      h4("Edge list preview:"),
-      tableOutput("esum"),
-      
-      # Horizontal line
-      tags$hr(),
-      
-      h4("Summary of most cited articles:"),
-      tableOutput("topCitations"),
-      
-      # Horizontal line
-      tags$hr(),
-      
-      downloadButton("downloadData", "Download full results")
+      column(
+        4,
+        tableOutput("resultsum"),
+        tags$hr(),
+        h4("Summary of most cited articles:"),
+        tableOutput("topCitations")
+      ),
+      column(
+        4,
+        h4("Edge list preview:"),
+        tableOutput("esum"),
+        downloadButton("downloadData", "Download full results")
+      )
     )
   ))
 
 # Define server logic
 server <- function(input, output) {
   # Code below only runs when 'submit' button is selected
+  
   observeEvent(input$action, {
-    
     # if there is no csv file upload, run text analysis
     if (is.null(input$file1)) {
       # reads in text input
       txtFile <- readLines(textConnection(input$txt))
+      
       
       # seperates text input by commas
       txtFile <- trimws(unlist(strsplit(txtFile, ",")))
@@ -92,13 +117,17 @@ server <- function(input, output) {
       analyzeIDs <- unique(uploadFile[[1]])
     }
     
-    # pass text input or csv input values to pmc2nc
+    
     set_entrez_key(input$entrezK)
-    pmids <- get_pmc_cited_in(analyzeIDs)
-    EL <- generateEdgeList(pmids)
+    
+    print(paste(input$choices, collapse = ", "))
+    
+    # pass text input or csv input values to pmc2nc
+    pmids <- get_pmc_cited_in(analyzeIDs, filter = "title")
+    EL <- generateEdgeList(pmids$res[[1]])
     
     ########################################################
-    # Output - first 6 PMIDs of Edge List shown (for testing)
+    # Output - first 6 PMIDs of Edge List shown
     output$esum <- renderTable({
       head(EL)
     })
@@ -114,7 +143,8 @@ server <- function(input, output) {
         "Summary" = c(
           "Number of articles in user list:",
           "Number cited by articles in PMC:",
-          "Number not cited or not in PMC:"),
+          "Number not cited or not in PMC:"
+        ),
         "Count" = c(res1, res2, res3)
       )
     
@@ -125,8 +155,10 @@ server <- function(input, output) {
     # get results for citation count -
     # convert edge list to data frame in order to construct table
     resl <- as.data.frame(getCitationCounts(EL))
+    colnames(resl) <- c("PMIDs", "Count")
     
-    output$topCitations <- renderTable(head(resl[order(resl$n, decreasing = TRUE),]))
+    output$topCitations <-
+      renderTable(head(resl[order(resl$Count, decreasing = TRUE), ]))
     
     ########################################################
     # allows user to download edge list
